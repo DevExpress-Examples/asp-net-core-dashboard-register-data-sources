7bc91051c2b3691e9bd68d66ae502227b3eaec0e
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-09-05 15:17:39 +0300

    add cs solution

---
+++ CS/WebDashboardDataSources/Category.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Xpo;
+
+namespace WebDashboardDataSources {
+    [Persistent("Categories"), DeferredDeletion(false)]
+    public class Category: XPCustomObject {
+        int categoryId;
+        string categoryName;
+        string description;
+        [Key]
+        public int CategoryID {
+            get { return categoryId; }
+            set { SetPropertyValue<int>("CategoryID", ref categoryId, value); }
+        }
+        public string CategoryName {
+            get { return categoryName; }
+            set { SetPropertyValue<string>("CategoryName", ref categoryName, value); }
+        }
+        public string Description {
+            get { return description; }
+            set { SetPropertyValue<string>("Description", ref description, value); }
+        }
+    }
+}
