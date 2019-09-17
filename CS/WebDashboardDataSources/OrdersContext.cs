15dda2bd0aa3ae0c0baf06bc9b5c8daf07893119
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-09-17 17:04:37 +0300

    add EF Core data source

---
+++ CS/WebDashboardDataSources/OrdersContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SQLite;
+
+namespace WebDashboardDataSources
+{
+    public partial class OrdersContext: DbContext
+    {
+        public OrdersContext() : base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=file:Data/nwind.db");
+        }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public long? EmployeeID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public long? ShipVia { get; set; }
+        public decimal? Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
--- CS/WebDashboardDataSources/Startup.cs
+++ CS/WebDashboardDataSources/Startup.cs
@@ -3,6 +3,7 @@ using DevExpress.DashboardAspNetCore;
 using DevExpress.DashboardCommon;
 using DevExpress.DashboardWeb;
 using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.EntityFramework;
 using DevExpress.DataAccess.Excel;
 using DevExpress.DataAccess.Json;
 using DevExpress.DataAccess.Sql;
@@ -33,70 +34,7 @@ namespace WebDashboardDataSources {
 
                     DashboardFileStorage dashboardFileStorage = new DashboardFileStorage(FileProvider.GetFileInfo("Data/Dashboards").PhysicalPath);
                     configurator.SetDashboardStorage(dashboardFileStorage);
-
-                    DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
-
-                    // Registers an SQL data source.
-                    DashboardSqlDataSource sqlDataSource = new DashboardSqlDataSource("SQL Data Source", "NWindConnectionString");
-                    sqlDataSource.DataProcessingMode = DataProcessingMode.Client;
-                    SelectQuery query = SelectQueryFluentBuilder
-                        .AddTable("Categories")
-                        .Join("Products", "CategoryID")
-                        .SelectAllColumns()
-                        .Build("Products_Categories");
-                    sqlDataSource.Queries.Add(query);
-                    dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml());
-
-                    // Registers an Object data source.
-                    DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("Object Data Source");
-                    dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
-
-                    // Registers an Excel data source.
-                    DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource("Excel Data Source");
-                    excelDataSource.FileName = FileProvider.GetFileInfo("Data/Sales.xlsx").PhysicalPath;
-                    excelDataSource.SourceOptions = new ExcelSourceOptions(new ExcelWorksheetSettings("Sheet1"));
-                    dataSourceStorage.RegisterDataSource("excelDataSource", excelDataSource.SaveToXml());
-
-                    // Registers an OLAP data source.
-                    DashboardOlapDataSource olapDataSource = new DashboardOlapDataSource("OLAP Data Source", "olapConnection");
-                    DashboardOlapDataSource.OlapDataProvider = OlapDataProviderType.Xmla;
-                    dataSourceStorage.RegisterDataSource("olapDataSource", olapDataSource.SaveToXml());
-
-                    // Registers an Extract data source.
-                    DashboardExtractDataSource extractDataSource = new DashboardExtractDataSource("Extract Data Source");
-                    extractDataSource.Name = "Extract Data Source";
-                    extractDataSource.FileName = FileProvider.GetFileInfo("Data/SalesPersonExtract.dat").PhysicalPath;
-                    dataSourceStorage.RegisterDataSource("extractDataSource ", extractDataSource.SaveToXml());
-
-                    // Registers a JSON data source from URL.
-                    DashboardJsonDataSource jsonDataSourceUrl = new DashboardJsonDataSource("JSON Data Source (URL)");
-                    jsonDataSourceUrl.JsonSource = new UriJsonSource(new Uri("https://raw.githubusercontent.com/DevExpress-Examples/DataSources/master/JSON/customers.json"));
-                    jsonDataSourceUrl.RootElement = "Customers";
-                    jsonDataSourceUrl.Fill();
-                    dataSourceStorage.RegisterDataSource("jsonDataSourceUrl", jsonDataSourceUrl.SaveToXml());
-
-                    // Registers a JSON data source from a JSON file.
-                    DashboardJsonDataSource jsonDataSourceFile = new DashboardJsonDataSource("JSON Data Source (File)");
-                    Uri fileUri = new Uri(FileProvider.GetFileInfo("Data/customers.json").PhysicalPath, UriKind.RelativeOrAbsolute);
-                    jsonDataSourceFile.JsonSource = new UriJsonSource(fileUri);
-                    jsonDataSourceFile.RootElement = "Customers";
-                    jsonDataSourceFile.Fill();
-                    dataSourceStorage.RegisterDataSource("jsonDataSourceFile", jsonDataSourceFile.SaveToXml());
-
-                    // Registers a JSON data source from JSON string.
-                    DashboardJsonDataSource jsonDataSourceString = new DashboardJsonDataSource("JSON Data Source (String)");
-                    string json = "{\"Customers\":[{\"Id\":\"ALFKI\",\"CompanyName\":\"Alfreds Futterkiste\",\"ContactName\":\"Maria Anders\",\"ContactTitle\":\"Sales Representative\",\"Address\":\"Obere Str. 57\",\"City\":\"Berlin\",\"PostalCode\":\"12209\",\"Country\":\"Germany\",\"Phone\":\"030-0074321\",\"Fax\":\"030-0076545\"}],\"ResponseStatus\":{}}";
-                    jsonDataSourceString.JsonSource = new CustomJsonSource(json);
-                    jsonDataSourceString.RootElement = "Customers";
-                    jsonDataSourceString.Fill();
-                    dataSourceStorage.RegisterDataSource("jsonDataSourceString", jsonDataSourceString.SaveToXml());
-                    configurator.SetDataSourceStorage(dataSourceStorage);
-
-                    // Registers an XPO data source.
-                    DashboardXpoDataSource xpoDataSource = new DashboardXpoDataSource("XPO Data Source");
-                    xpoDataSource.ConnectionStringName = "NWindConnectionString";
-                    xpoDataSource.SetEntityType(typeof(Category));
-                    dataSourceStorage.RegisterDataSource("xpoDataSource", xpoDataSource.SaveToXml());
+                    configurator.SetDataSourceStorage(CreateDataSourceStorage());
 
                     configurator.DataLoading += (s, e) => {
                         if(e.DataSourceName == "Object Data Source") {
@@ -104,10 +42,82 @@ namespace WebDashboardDataSources {
                         }
                     };
 
-                    configurator.ConfigureDataConnection += Configurator_ConfigureDataConnection; ;
+                    configurator.ConfigureDataConnection += Configurator_ConfigureDataConnection;
                 });
 
             services.AddDevExpressControls(options => options.Resources = ResourcesType.ThirdParty | ResourcesType.DevExtreme);
+        }
+
+        public DataSourceInMemoryStorage CreateDataSourceStorage() {
+            DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+
+            // Registers an SQL data source.
+            DashboardSqlDataSource sqlDataSource = new DashboardSqlDataSource("SQL Data Source", "NWindConnectionString");
+            sqlDataSource.DataProcessingMode = DataProcessingMode.Client;
+            SelectQuery query = SelectQueryFluentBuilder
+                .AddTable("Categories")
+                .Join("Products", "CategoryID")
+                .SelectAllColumns()
+                .Build("Products_Categories");
+            sqlDataSource.Queries.Add(query);
+            dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml());
+
+            // Registers an OLAP data source.
+            DashboardOlapDataSource olapDataSource = new DashboardOlapDataSource("OLAP Data Source", "olapConnection");
+            DashboardOlapDataSource.OlapDataProvider = OlapDataProviderType.Xmla;
+            dataSourceStorage.RegisterDataSource("olapDataSource", olapDataSource.SaveToXml());
+
+            // Registers an Excel data source.
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource("Excel Data Source");
+            excelDataSource.FileName = FileProvider.GetFileInfo("Data/Sales.xlsx").PhysicalPath;
+            excelDataSource.SourceOptions = new ExcelSourceOptions(new ExcelWorksheetSettings("Sheet1"));
+            dataSourceStorage.RegisterDataSource("excelDataSource", excelDataSource.SaveToXml());
+
+            // Registers an Object data source.
+            DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("Object Data Source");
+            dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
+
+            // Registers a EF Core data source.
+            DashboardEFDataSource efDataSource = new DashboardEFDataSource("EF Core Data Source");
+            efDataSource.ConnectionParameters = new EFConnectionParameters(typeof(OrdersContext));
+            dataSourceStorage.RegisterDataSource("efDataSource", efDataSource.SaveToXml());
+
+            // Registers an Extract data source.
+            DashboardExtractDataSource extractDataSource = new DashboardExtractDataSource("Extract Data Source");
+            extractDataSource.Name = "Extract Data Source";
+            extractDataSource.FileName = FileProvider.GetFileInfo("Data/SalesPersonExtract.dat").PhysicalPath;
+            dataSourceStorage.RegisterDataSource("extractDataSource ", extractDataSource.SaveToXml());
+            
+            // Registers a JSON data source from URL.
+            DashboardJsonDataSource jsonDataSourceUrl = new DashboardJsonDataSource("JSON Data Source (URL)");
+            jsonDataSourceUrl.JsonSource = new UriJsonSource(new Uri("https://raw.githubusercontent.com/DevExpress-Examples/DataSources/master/JSON/customers.json"));
+            jsonDataSourceUrl.RootElement = "Customers";
+            jsonDataSourceUrl.Fill();
+            dataSourceStorage.RegisterDataSource("jsonDataSourceUrl", jsonDataSourceUrl.SaveToXml());
+
+            // Registers a JSON data source from a JSON file.
+            DashboardJsonDataSource jsonDataSourceFile = new DashboardJsonDataSource("JSON Data Source (File)");
+            Uri fileUri = new Uri(FileProvider.GetFileInfo("Data/customers.json").PhysicalPath, UriKind.RelativeOrAbsolute);
+            jsonDataSourceFile.JsonSource = new UriJsonSource(fileUri);
+            jsonDataSourceFile.RootElement = "Customers";
+            jsonDataSourceFile.Fill();
+            dataSourceStorage.RegisterDataSource("jsonDataSourceFile", jsonDataSourceFile.SaveToXml());
+
+            // Registers a JSON data source from JSON string.
+            DashboardJsonDataSource jsonDataSourceString = new DashboardJsonDataSource("JSON Data Source (String)");
+            string json = "{\"Customers\":[{\"Id\":\"ALFKI\",\"CompanyName\":\"Alfreds Futterkiste\",\"ContactName\":\"Maria Anders\",\"ContactTitle\":\"Sales Representative\",\"Address\":\"Obere Str. 57\",\"City\":\"Berlin\",\"PostalCode\":\"12209\",\"Country\":\"Germany\",\"Phone\":\"030-0074321\",\"Fax\":\"030-0076545\"}],\"ResponseStatus\":{}}";
+            jsonDataSourceString.JsonSource = new CustomJsonSource(json);
+            jsonDataSourceString.RootElement = "Customers";
+            jsonDataSourceString.Fill();
+            dataSourceStorage.RegisterDataSource("jsonDataSourceString", jsonDataSourceString.SaveToXml());
+
+            // Registers an XPO data source.
+            DashboardXpoDataSource xpoDataSource = new DashboardXpoDataSource("XPO Data Source");
+            xpoDataSource.ConnectionStringName = "NWindConnectionString";
+            xpoDataSource.SetEntityType(typeof(Category));
+            dataSourceStorage.RegisterDataSource("xpoDataSource", xpoDataSource.SaveToXml());
+
+            return dataSourceStorage;
         }
 
         private void Configurator_ConfigureDataConnection(object sender, ConfigureDataConnectionWebEventArgs e) {
